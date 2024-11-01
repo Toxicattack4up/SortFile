2dcfe30df11ef3e169b9ca5d03045ac5b311a30c
Леонид Кухарчик <kuxarchik007@mail.ru>
2024-11-01 19:13:56 +0300

    Добавьте файлы проекта.

---
+++ SortFile.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <fstream>  // работа с файлами
+#include <iostream>
+#include <cstdlib> // для получения рандомных чисел
+#include <ctime>
+
+using namespace std;
+
+void read_file()
+{
+
+}
+
+void write_file()
+{
+
+}
+
+void sort_file()
+{
+
+}
+
+int main()
+{
+    
+}
